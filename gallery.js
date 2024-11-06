aba567bcbd229693071e888f4e4b82a0b114290f
AaronM524 <aaronmathew468@gmail.com>
2024-11-06 11:01:55 -0500

    Add custom JSON file with image metadata

--- gallery.js
+++ gallery.js
@@ -1,6 +1,6 @@
 let mCurrentIndex = 0 // Tracks the current image index
 let mImages = [] // Array to hold GalleryImage objects
-const mUrl = 'https://your-json-url.com' // Replace with actual JSON URL
+const mUrl = 'https://api.npoint.io/b4b45452c9c3fbeb9af4' // Replace with actual JSON URL
 const mWaitTime = 5000 // Timer interval in milliseconds
 
 $(document).ready(() => {
