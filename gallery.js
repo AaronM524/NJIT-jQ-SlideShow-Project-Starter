5c0f607cfdf01261ee1a93480d9002f390b1c786
AaronM524 <aaronmathew468@gmail.com>
2024-11-15 23:01:28 -0500

    Implement next and previous button functionality

--- gallery.js
+++ gallery.js
@@ -1,6 +1,6 @@
 let mCurrentIndex = 0 // Tracks the current image index
 let mImages = [] // Array to hold GalleryImage objects
-const mUrl = 'https://api.npoint.io/689d577e91bf27489a4d' // Replace with actual JSON URL
+const mUrl = 'https://api.npoint.io/52f25d0de2b5e6a9a5ee' // Replace with actual JSON URL
 const mWaitTime = 5000 // Timer interval in milliseconds
 
 $(document).ready(() => {
@@ -13,12 +13,11 @@ $(document).ready(() => {
   // - slideToggle the visibility of the .details section
   $('.moreIndicator').on('click', () => {
     $('.details').slideToggle();
-
   })
   // Select the "Next Photo" button and add a click event to call showNextPhoto
-
+  $('#nextPhoto').on('click', showNextPhoto);
   // Select the "Previous Photo" button and add a click event to call showPrevPhoto
-
+  $('#prevPhoto').on('click', showPrevPhoto);
   // Call fetchJSON() to load the initial set of images
   fetchJSON()
 })
@@ -53,13 +52,20 @@ function swapPhoto() {
 
 // Advances to the next photo, loops to the first photo if the end of array is reached
 function showNextPhoto () {
-  // Increment mCurrentIndex and call swapPhoto()
-  // Ensure it loops back to the beginning if mCurrentIndex exceeds array length
+  mCurrentIndex++;
+  if (mCurrentIndex == mImages.length) {
+    mCurrentIndex = 0;
+  }
+  swapPhoto()
 }
 // Goes to the previous photo, loops to the last photo if mCurrentIndex goes negative
 function showPrevPhoto () {
-  // Decrement mCurrentIndex and call swapPhoto()
-  // Ensure it loops to the end if mCurrentIndex is less than 0
+  mCurrentIndex--;
+  if (mCurrentIndex < 0) {
+    mCurrentIndex = mImages.length - 1; 
+  }
+  
+  swapPhoto();
 }
 
 // Starter code for the timer function
